dc68fdfc422b4caf65d1bd7a6bf949d68184ca14
Bartosz Olszanowski <bartosz.olszanowski@gmail.com>
2016-04-19 13:55:41 +0200

    Added support for 3D Touch shortcut items.

--- wwdc16/AppDelegate.swift
+++ wwdc16/AppDelegate.swift
@@ -10,35 +10,98 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: Properties
 
     var window: UIWindow?
-
+    var shortcutItem: UIApplicationShortcutItem?
+    
+    // MARK: App delegate main methods
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if let shortcutItem = shortcutItem {
+            handleShortcut(shortcutItem)
+            self.shortcutItem = nil
+        }
     }
 
     func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    // MARK: Shortcut items
+    
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void) {
+        completionHandler(handleShortcut(shortcutItem))
+    }
+    
+    func handleShortcut(shortcutItem: UIApplicationShortcutItem) -> Bool {
+        switch shortcutItem.type {
+        case ShortcutItem.MyApps:
+            handleMyAppsShortcut()
+            return true
+        case ShortcutItem.AboutMe:
+            handleAboutMeShortcut()
+            return true
+        case ShortcutItem.Interests:
+            handleInterestsShortcut()
+            return true
+        case ShortcutItem.Skills:
+            handleSkillsShortcut()
+            return true
+        default:
+            break
+        }
+        return false
+    }
+    
+    func checkIfShortcutItemTriggeredApplicationStartWithLaunchOptions(launchOptions: NSDictionary) -> Bool {
+        var performShortcutDelegate = true
+        if let shortcutItem = launchOptions.objectForKey(UIApplicationLaunchOptionsShortcutItemKey) as? UIApplicationShortcutItem {
+            self.shortcutItem = shortcutItem
+            performShortcutDelegate = false
+            switch shortcutItem.type {
+            case ShortcutItem.MyApps:
+                handleMyAppsShortcut()
+            case ShortcutItem.AboutMe:
+                handleAboutMeShortcut()
+            case ShortcutItem.Interests:
+                handleInterestsShortcut()
+            case ShortcutItem.Skills:
+                handleSkillsShortcut()
+            default:
+                break
+            }
+        }
+        return performShortcutDelegate
+    }
+    
+    func handleMyAppsShortcut() {
+        print("3D Touch: My Apps pressed.")
+    }
+    
+    func handleAboutMeShortcut() {
+        print("3D Touch: About me pressed.")
+    }
+    
+    func handleInterestsShortcut() {
+        print("3D Touch: Interests pressed.")
+    }
+    
+    func handleSkillsShortcut() {
+        print("3D Touch: Skills pressed.")
     }
 
 
---
+++ wwdc16/Supporting Files/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  wwdc16
+//
+//  Created by Bartosz Olszanowski on 19.04.2016.
+//  Copyright © 2016 Bartosz Olszanowski. All rights reserved.
+//
+
+import Foundation
+
+
+struct ShortcutItem {
+    static let MyApps = "com.olbart.wwdc16.my-apps"
+    static let AboutMe = "com.olbart.wwdc16.about-me"
+    static let Interests = "com.olbart.wwdc16.interests"
+    static let Skills = "com.olbart.wwdc16.skills"
+}
+
+

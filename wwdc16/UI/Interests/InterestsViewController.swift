cd6dfb671c907628928f10c92b90004e1c74f81c
Bartosz Olszanowski <bartosz.olszanowski@gmail.com>
2016-04-24 21:18:45 +0200

    InterestsVC: header animation added

--- wwdc16/UI/Interests/InterestsViewController.swift
+++ wwdc16/UI/Interests/InterestsViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
+import PhotosUI
 
 class InterestsViewController: PresentedViewController {
+    
+    // MARK: Properties
+    
+    private struct Constants {
+        static let CloseButtonOffsetFromRightEdge: CGFloat = 20.0 + 50.0
+        static let Headers = ["Snowboard", "Travelling", "Sillicon Valley"]
+    }
+    
+    var headerViewAnimators = [BOTextAnimator]()
+    let viewWidth = UIScreen.mainScreen().bounds.size.width
+    
+    @IBOutlet var livePhotoViews: [PHLivePhotoView]! {
+        didSet {
+            livePhotoViews.sortInPlace { (firstView, secondView) -> Bool in
+                return firstView.tag < secondView.tag
+            }
+        }
+    }
+    @IBOutlet var headerViews: [UIView]! {
+        didSet {
+            headerViews.sortInPlace { (firstView, secondView) -> Bool in
+                return firstView.tag < secondView.tag
+            }
+        }
+    }
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var closeButtonLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var viewWidthConstraint: NSLayoutConstraint!
     
     // MARK: VC' Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .themeAzureColor()
+        closeButtonLeadingConstraint.constant = view.bounds.size.width - Constants.CloseButtonOffsetFromRightEdge
+        viewWidthConstraint.constant = viewWidth
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        view.setNeedsLayout()
+        view.updateConstraintsIfNeeded()
+        setupTextAnimators()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let index = Int(scrollView.contentOffset.x) / Int(viewWidth)
+        headerViewAnimators[index].updatePathStrokeWithValue(1.0)
+    }
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        clearTextAnimators()
+    }
+    
+    // MARK: Appearance
+    
+    func setupTextAnimators() {
+        for (index, headerView) in headerViews.enumerate() {
+            let textAnimator = BOTextAnimator(referenceView: headerView)
+            textAnimator.textToAnimate = Constants.Headers[index]
+            textAnimator.prepareForAnimation()
+            headerViewAnimators.append(textAnimator)
+        }
+    }
+    
+    func clearTextAnimators() {
+        headerViewAnimators.removeAll()
     }
     
     // MARK: Actions
@@ -23,4 +87,21 @@ class InterestsViewController: PresentedViewController {
         dismissViewControllerWithoutAnimation()
     }
 
+}
+
+// MARK: UIScrollView delegate
+
+extension InterestsViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let contentOffsetX = scrollView.contentOffset.x
+        print("\(contentOffsetX)")
+        for (index, headerViewAnimator) in headerViewAnimators.enumerate() {
+            let startOffsetX = viewWidth * (CGFloat(index) - 0.5)
+            let endOffsetX = startOffsetX + 0.5 * viewWidth
+            if contentOffsetX >= startOffsetX && contentOffsetX <= endOffsetX {
+                let strokeValue = (contentOffsetX - startOffsetX) / (0.5 * viewWidth)
+                headerViewAnimator.updatePathStrokeWithValue(strokeValue)
+            }
+        }
+    }
 }
---
+++ wwdc16/Utilities/BOTextAnimator.swift
@@ -0,0 +1,162 @@
+//
+//  BOTextAnimator.swift
+//  BOTextAnimator
+//
+//  Created by Bartosz Olszanowski on 13.04.2016.
+//  Copyright © 2016 olbart. All rights reserved.
+//
+
+import UIKit
+import CoreFoundation
+
+public protocol BOTextAnimatorDelegate {
+    func textAnimator(textAnimator: BOTextAnimator, animationDidStart animation: CAAnimation)
+    func textAnimator(textAnimator: BOTextAnimator, animationDidStop animation: CAAnimation)
+}
+
+public class BOTextAnimator: NSObject {
+    
+    // MARK: Properties
+    public var fontName         = "Avenir-Roman"
+    public var fontSize         : CGFloat = 50.0
+    public var textToAnimate    = "Hello Swift!"
+    public var textColor        = UIColor.redColor().CGColor
+    public var delegate         : BOTextAnimatorDelegate?
+    
+    private var animationLayer  = CALayer()
+    private var pathLayer       : CAShapeLayer?
+    private var referenceView   : UIView
+    
+    // MARK: Initialization
+    init(referenceView: UIView) {
+        self.referenceView          = referenceView
+        super.init()
+        defaultConfiguration()
+    }
+    
+    deinit {
+        clearLayer()
+    }
+    
+    // MARK: Configuration
+    private func defaultConfiguration() {
+        animationLayer          = CALayer()
+        animationLayer.frame    = referenceView.bounds
+        referenceView.layer.addSublayer(animationLayer)
+        setupPathLayerWithText(textToAnimate, fontName: fontName, fontSize: fontSize)
+    }
+
+    // MARK: Animations
+    
+    private func clearLayer() {
+        if let _ = pathLayer {
+            pathLayer?.removeFromSuperlayer()
+            pathLayer = nil
+        }
+    }
+    
+    private func setupPathLayerWithText(text: String, fontName: String, fontSize: CGFloat) {
+        clearLayer()
+        
+        let letters     = CGPathCreateMutable()
+        let font        = CTFontCreateWithName(fontName, fontSize, nil)
+        let attrString  = NSAttributedString(string: text, attributes: [kCTFontAttributeName as String : font])
+        let line        = CTLineCreateWithAttributedString(attrString)
+        let runArray    = CTLineGetGlyphRuns(line)
+        
+        for runIndex in 0..<CFArrayGetCount(runArray) {
+            
+            let run     : CTRunRef =  unsafeBitCast(CFArrayGetValueAtIndex(runArray, runIndex), CTRunRef.self)
+            let dictRef : CFDictionaryRef = unsafeBitCast(CTRunGetAttributes(run), CFDictionaryRef.self)
+            let dict    : NSDictionary = dictRef as NSDictionary
+            let runFont = dict[kCTFontAttributeName as String] as! CTFont
+            
+            for runGlyphIndex in 0..<CTRunGetGlyphCount(run) {
+                let thisGlyphRange  = CFRangeMake(runGlyphIndex, 1)
+                var glyph           = CGGlyph()
+                var position        = CGPointZero
+                CTRunGetGlyphs(run, thisGlyphRange, &glyph)
+                CTRunGetPositions(run, thisGlyphRange, &position)
+                
+                let letter          = CTFontCreatePathForGlyph(runFont, glyph, nil)
+                var t               = CGAffineTransformMakeTranslation(position.x, position.y)
+                CGPathAddPath(letters, &t, letter)
+            }
+        }
+        
+        let path = UIBezierPath()
+        path.moveToPoint(CGPointZero)
+        path.appendPath(UIBezierPath(CGPath: letters))
+        
+        let pathLayer               = CAShapeLayer()
+        pathLayer.frame             = animationLayer.bounds;
+        pathLayer.bounds            = CGPathGetBoundingBox(path.CGPath)
+        pathLayer.geometryFlipped   = true
+        pathLayer.path              = path.CGPath
+        pathLayer.strokeColor       = textColor
+        pathLayer.fillColor         = nil
+        pathLayer.lineWidth         = 1.0
+        pathLayer.lineJoin          = kCALineJoinBevel
+        
+        self.animationLayer.addSublayer(pathLayer)
+        self.pathLayer = pathLayer
+        
+    }
+    
+    public func startAnimation() {
+        let duration = 4.0
+        pathLayer?.removeAllAnimations()
+        setupPathLayerWithText(textToAnimate, fontName: fontName, fontSize: fontSize)
+        
+        let pathAnimation       = CABasicAnimation(keyPath: "strokeEnd")
+        pathAnimation.duration  = duration
+        pathAnimation.fromValue = 0.0
+        pathAnimation.toValue   = 1.0
+        pathAnimation.delegate  = self
+        pathLayer?.addAnimation(pathAnimation, forKey: "strokeEnd")
+        
+        let coloringDuration        = 2.0
+        let colorAnimation          = CAKeyframeAnimation(keyPath: "fillColor")
+        colorAnimation.duration     = duration + coloringDuration
+        colorAnimation.values       = [UIColor.clearColor().CGColor, UIColor.clearColor().CGColor, textColor]
+        colorAnimation.keyTimes     = [0, (duration/(duration + coloringDuration)), 1]
+        pathLayer?.addAnimation(colorAnimation, forKey: "fillColor")
+    }
+    
+    public func stopAnimation() {
+        pathLayer?.removeAllAnimations()
+    }
+    
+    public func clearAnimationText() {
+        clearLayer()
+    }
+    
+    public func prepareForAnimation() {
+        pathLayer?.removeAllAnimations()
+        setupPathLayerWithText(textToAnimate, fontName: fontName, fontSize: fontSize)
+        
+        let pathAnimation       = CABasicAnimation(keyPath: "strokeEnd")
+        pathAnimation.duration  = 1.0
+        pathAnimation.fromValue = 0.0
+        pathAnimation.toValue   = 1.0
+        pathAnimation.delegate  = self
+        pathLayer?.addAnimation(pathAnimation, forKey: "strokeEnd")
+        
+        pathLayer?.speed        = 0
+        
+    }
+    
+    public func updatePathStrokeWithValue(value: CGFloat) {
+        pathLayer?.timeOffset = CFTimeInterval(value)
+    }
+    
+    // MARK: Animation delegate
+    public override func animationDidStart(anim: CAAnimation) {
+        self.delegate?.textAnimator(self, animationDidStart: anim)
+    }
+    
+    public override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        self.delegate?.textAnimator(self, animationDidStop: anim)
+    }
+
+}
